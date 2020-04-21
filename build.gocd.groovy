7e5faa08936a8aca079f67d9d1952fb4a2ea407c
Marques Lee <marques.lee@thoughtworks.com>
2020-04-20 22:30:53 -0700

    Update copyright

--- build.gocd.groovy
+++ build.gocd.groovy
@@ -1,7 +1,5 @@
-import cd.go.contrib.plugins.configrepo.groovy.dsl.GoCD
-
 /*
- * Copyright 2018 ThoughtWorks, Inc.
+ * Copyright 2020 ThoughtWorks, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +13,8 @@ import cd.go.contrib.plugins.configrepo.groovy.dsl.GoCD
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import cd.go.contrib.plugins.configrepo.groovy.dsl.GoCD
 
 GoCD.script {
   pipelines {
